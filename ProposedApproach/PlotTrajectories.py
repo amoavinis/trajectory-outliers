66300e5bd6a42f133c6def8e11c592ca0d1cc432
Angelos Moavinis <amoavinis@gmail.com>
2022-04-22 18:54:54 +0300

    plot trajectories

---
+++ ProposedApproach/PlotTrajectories.py
@@ -0,0 +1,27 @@
+import pickle
+from random import sample
+from matplotlib import pyplot as plt
+
+data = pickle.load(open("trajectories_labeled_geolife.pkl", "rb"))
+X = [[p[:2] for p in d[0]] for d in data]
+y = [d[1] for d in data]
+
+inliers = [X[i] for i in range(len(X)) if y[i]==0]
+outliers = [X[i] for i in range(len(X)) if y[i]==1]
+
+sample_inliers = sample(inliers, 500)
+sample_outliers = sample(outliers, 50)
+
+def plot_one(t, color):
+    c1 = [s[0] for s in t]
+    c2 = [s[1] for s in t]
+    plt.plot(c1, c2, color=color)
+
+def plot_group(X, color):
+    for x in X:
+        plot_one(x, color)
+
+plot_group(sample_inliers, 'b')
+plot_group(sample_outliers, 'r')
+
+plt.show()
