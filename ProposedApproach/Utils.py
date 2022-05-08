5f1f808d5310f4efd0f4a667af3a52006c00f64f
Angelos Moavinis <amoavinis@gmail.com>
2022-05-08 17:24:19 +0300

    round

--- ProposedApproach/Utils.py
+++ ProposedApproach/Utils.py
@@ -3,7 +3,7 @@ import numpy as np
 def average_length_of_sequences(sequences):
     s = sum([len(x) for x in sequences])
     avg = s / len(sequences)
-    return avg
+    return round(avg, 2)
 
 def distance_of_trajectory(traj):
     dist = 0.0
