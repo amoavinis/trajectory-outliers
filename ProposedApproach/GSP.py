ebc194caf654fed7ba357cebca00f01f14365f28
Angelos Moavinis <amoavinis@gmail.com>
2022-05-31 10:14:15 +0300

    gsp fix

--- ProposedApproach/GSP.py
+++ ProposedApproach/GSP.py
@@ -6,18 +6,10 @@ from hausdorff import hausdorff_distance
 
 class GSPModule:
     def __init__(self):
-        pass
+        self.freq_subsequences = []
 
     def stringify_grid_trajectory(self, trajectory):
         return [f"{p}" for p in trajectory]
-
-    def find_frequent_subsequences(self, X, t):
-        output = GSP(X).search(t)
-        freq_subsequences = []
-        for d in output:
-            freq_subsequences.extend(d.keys())
-
-        return freq_subsequences
 
     def intify_grid_trajectories(self, X):
         intified = []
@@ -30,22 +22,25 @@ class GSPModule:
 
         return intified
 
-    def deviation_from_frequent(self, X, t):
-        stringified = [self.stringify_grid_trajectory(x) for x in X]
-
+    def find_frequent_subsequences(self, X, t, read_pkl=False):
         frequent = []
-        if os.path.exists("freq_subs.pkl"):
-            dicts = pickle.load(open("freq_subs.pkl", "rb"))
-            for d in dicts:
-                frequent.extend(d.keys())
+        if read_pkl and os.path.exists(f"freq_subs_{str(t).replace('.', '')}.pkl"):
+            frequent = pickle.load(open(f"freq_subs_{str(t).replace('.', '')}.pkl", "rb"))   
         else:
-            frequent = self.find_frequent_subsequences(stringified, t)
-            pickle.dump(frequent, open("freq_subs.pkl", "wb"))
+            stringified = [self.stringify_grid_trajectory(x) for x in X]
+            output = GSP(stringified).search(t)
+            for d in output:
+                frequent.extend(d.keys())
+            pickle.dump(frequent, open(f"freq_subs_{str(t).replace('.', '')}.pkl", "wb"))
         intified_freq = self.intify_grid_trajectories(frequent)
-        distances = np.zeros((len(X), len(frequent)))
+        
+        self.freq_subsequences = intified_freq
+
+    def deviation_from_frequent(self, X):
+        distances = np.zeros((len(X), len(self.freq_subsequences)))
         for i in range(distances.shape[0]):
             for j in range(distances.shape[1]):
-                distances[i, j] = hausdorff_distance(X[i], intified_freq[j])
+                distances[i, j] = hausdorff_distance(X[i], self.freq_subsequences[j])
 
         mins = np.amin(distances, axis=1)
 
