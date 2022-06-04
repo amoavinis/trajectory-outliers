234b2a0ed30f4e384d489299f14081fae4f9fea6
Angelos Moavinis <amoavinis@gmail.com>
2022-06-04 07:45:35 +0300

    fix

--- ProposedApproach/AutomaticLabeling.py
+++ ProposedApproach/AutomaticLabeling.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import json
 from sklearn.metrics import silhouette_score
 from fastcluster import linkage
 from scipy.cluster.hierarchy import fcluster
@@ -23,6 +24,7 @@ class Normalizer:
         file = open(f, 'r')
         if self.dataset == "geolife":
             lines = file.readlines()[6:]
+            file.close()
 
             result = []
 
@@ -40,9 +42,19 @@ class Normalizer:
         else:
             print("Not implemented")
 
+    def process_skg_json(self, json_text):
+        obj = json.loads(json_text)
+        traj_raw = obj["signals"]
+        traj = []
+        for p in traj_raw:
+            latitude = p["latitude"]/10**5
+            longitude = p["longitude"]/10**5
+            traj.append([longitude, latitude])
+        return traj
+
     def create_trajectories(self):
-        for i in tqdm(os.listdir(self.data_path)):
-            if self.dataset == "geolife":
+        if self.dataset == "geolife":
+            for i in tqdm(os.listdir(self.data_path)):
                 i_path = self.data_path+i+"/Trajectory/"
                 for j in os.listdir(i_path):
                     trajectory = self.process_file(i_path+j)
@@ -53,8 +65,21 @@ class Normalizer:
                             break
                     if valid and len(trajectory) > 0:
                         self.all_trajectories.append(trajectory)
-            else:
-                print("Not implemented")
+        elif self.dataset == "thessaloniki":
+            file =  open(self.data_path+"decoded.txt", "r")
+            lines = file.readlines()
+            file.close()
+
+            self.all_trajectories = [self.process_skg_json(line) for line in lines]
+            print(len(self.all_trajectories))
+            points = []
+            for t in self.all_trajectories:
+                for p in t:
+                    points.append(p)
+            plt.scatter([p[0] for p in points], [p[1] for p in points])
+            plt.show()
+        else:
+            print("Not implemented")
 
     def take_points(self):
         all_points = []
@@ -73,14 +98,14 @@ class Normalizer:
     def remove_duplicates(self, x):
         y = [x[0]]
         for i in range(1, len(x)):
-            if y[-1][0] != x[i][0]:
+            if y[-1] != x[i]:
                 y.append(x[i])
         return y
 
     def transform_trajectory_to_grid(self, traj, grid_scale):
         trajectory_transformed = []
         for p in traj:
-            to_append = (self.coords_to_grid(self.scaler.transform([p[:2]])[0], grid_scale), p[2])
+            to_append = self.coords_to_grid(self.scaler.transform([p[:2]])[0], grid_scale)
             trajectory_transformed.append(to_append)
         return self.remove_duplicates(trajectory_transformed)
 
@@ -131,7 +156,7 @@ class Normalizer:
 
 data_prefixes = {
     "geolife": "Datasets/Geolife Trajectories 1.3/Data/",
-    "thessaloniki": "TODO"
+    "thessaloniki": "Datasets/ThessalonikiDataset/"
 }
 
 class Labeling:
@@ -181,11 +206,13 @@ class Labeling:
         print("Total number of trajectories:", len(self.all_trajectories))
         print("Number of step 1 outliers:", len(self.outliers))
         score = []
+
         for k in filtered_sd:
             self.paths = [f[1] for f in filtered_sd[k]]
             to_cluster = [[i] for i in range(len(self.paths))]
             linked = linkage(to_cluster, method='complete', metric=self.custom_distance)
             clusters = fcluster(linked, t=self.dist_clustering, criterion='distance')
+
             if len(set(clusters)) >= 2 and len(set(clusters)) <= len(to_cluster)-1:
                 silhouette_score_1 = silhouette_score(to_cluster, clusters, metric=self.custom_distance)
                 score.append(silhouette_score_1)
@@ -212,9 +239,7 @@ class Labeling:
         pickle.dump(res, open(os.getcwd()+"/trajectories_labeled_"+self.dataset+".pkl", 'wb'))
 
     def start(self, data):
-        print("Reading trajectories from disk...")
         self.all_trajectories = data
-        print("Read trajectories from disk.")
         print("Clustering trajectories...")
         self.clustering_trajectories()
         print("Clustered trajectories.")
@@ -228,14 +253,14 @@ parser.add_argument("--dataset", help="Specify the dataset to use", default="geo
 parser.add_argument("--G", help="The number of grid cells per dimension", default="5")
 parser.add_argument("--thr", help="Percentage threshold for acceptable cluster size.", default="0.03")
 parser.add_argument("--minThr", help="Count threshold for acceptable sd-pair size.", default="2")
-parser.add_argument("--dist", help="The distance threshold for forming clusters with the complete linkage algorithm.", default="0.8")
+parser.add_argument("--dist", help="The distance threshold for forming clusters with the complete linkage algorithm.", default="0.4")
 args = parser.parse_args()
 
 dataset = args.dataset
 thr = float(args.thr)
 minThr = int(args.minThr)
 distClustering = float(args.dist)
-grid_scale = int(args.gridScale)
+grid_scale = int(args.G)
 
 nm = Normalizer(dataset, os.getcwd()+"/"+data_prefixes[dataset], grid_scale)
 nm.preprocess()
