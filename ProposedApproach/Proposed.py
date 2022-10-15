5c3b4d24de3e09f242ea244ab4cb64625381b260
Angelos Moavinis <amoavinis@gmail.com>
2022-10-15 17:07:20 +0300

    WIP

--- ProposedApproach/Proposed.py
+++ ProposedApproach/Proposed.py
@@ -1,3 +1,13 @@
+from GSP import GSPModule
+from Utils import average_length_of_sequences, distance_of_trajectory
+from CustomScaler import Scaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import f1_score, accuracy_score, confusion_matrix
+import numpy as np
 import pickle
 import argparse
 from tqdm import tqdm
@@ -6,17 +16,6 @@ from numba import njit
 import warnings
 warnings.filterwarnings("ignore")
 
-import numpy as np
-from sklearn.metrics import f1_score, accuracy_score, confusion_matrix
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.cluster import DBSCAN
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-
-from CustomScaler import Scaler
-from Utils import average_length_of_sequences, distance_of_trajectory
-from GSP import GSPModule
 
 @njit
 def hausdorff_dist(A, B):
@@ -31,6 +30,7 @@ def hausdorff_dist(A, B):
             dist = minimum
     return dist
 
+
 @njit
 def calculate_distances(X):
     distances = np.zeros((len(X), len(X)))
@@ -40,6 +40,7 @@ def calculate_distances(X):
             distances[i, j] = d
             distances[j, i] = d
     return distances
+
 
 parser = argparse.ArgumentParser(
     description="Compare performance of clustering algorithms.")
@@ -74,7 +75,8 @@ data = pickle.load(open(data_file, "rb"))
 X_init = [[p[:2] for p in d[0]] for d in data]
 y = np.array([d[1] for d in data])
 
-x_init_train, x_init_test, y_train, y_test = train_test_split(X_init, y, train_size=0.75, random_state=seed)
+x_init_train, x_init_test, y_train, y_test = train_test_split(
+    X_init, y, train_size=0.75, random_state=seed)
 
 print("Average length of raw sequences:", average_length_of_sequences(X_init))
 
@@ -94,7 +96,7 @@ X_grid_test = []
 for x in x_train:
     X_grid_train.append(scaler.trajectory_to_grid(x, grid_scale))
 print("Average length of size " + str(grid_scale) + " grid cell sequences:",
-    average_length_of_sequences(X_grid_train))
+      average_length_of_sequences(X_grid_train))
 
 gsp = GSPModule()
 if method != "cluster" and do_gsp:
@@ -106,27 +108,36 @@ for x in x_test:
 if method != "svm":
     distances = calculate_distances(X_grid_train+X_grid_test)
     distances_train = distances[:len(X_grid_train), :len(X_grid_train)]
-    
+
     dbscan = DBSCAN(eps=eps, metric="precomputed", n_jobs=-1, min_samples=2)
     labels_train = dbscan.fit_predict(distances_train)
 
     distances_test_pred = distances[len(X_grid_train):, :len(X_grid_train)]
-    labels_test = [labels_train[np.argmin(distances_test_pred[i])] for i in range(len(X_grid_test))]
+    labels_test = [labels_train[np.argmin(
+        distances_test_pred[i])] for i in range(len(X_grid_test))]
 
     y_pred_train1 = np.array([1 if l == -1 else 0 for l in labels_train])
     y_pred_test1 = np.array([1 if l == -1 else 0 for l in labels_test])
     print("Finished path clustering")
 
-def calc_features(X, gsp_dists=[], gsp=False):
+
+def calc_features(X, gsp_dists=[], gsp=False, isCoordinates=False):
     feature_list = []
+    # FEATURES: MAYBE ADD TRIP TIME
     for i, x in enumerate(X):
         features = [x[0][0], x[0][1], x[-1][0], x[-1][1],
-            distance_of_trajectory(x)]
+                    distance_of_trajectory(np.array(x), isCoordinates=isCoordinates)]
         if gsp:
             features.append(gsp_dists[i])
         feature_list.append(features)
     return feature_list
 
+
+x_train_features = np.array(calc_features(x_init_train, isCoordinates=True))
+minmax_values = [(np.min(x_train_features[:, j]), np.max(
+    x_train_features[:, j])) for j in range(5)]
+
+pickle.dump(minmax_values, open(dataset+"_minmax.pkl", "wb"))
 
 if method != "cluster":
     gsp_dists_train = []
@@ -134,10 +145,10 @@ if method != "cluster":
     if do_gsp:
         gsp_dists_train = gsp.deviation_from_frequent(X_grid_train)
         gsp_dists_test = gsp.deviation_from_frequent(X_grid_test)
-    
+
     X_features_train = calc_features(x_train, gsp_dists_train, do_gsp)
     X_features_test = calc_features(x_test, gsp_dists_test, do_gsp)
-    
+
     minmax = MinMaxScaler()
     X_features_train = minmax.fit_transform(X_features_train)
     X_features_test = minmax.transform(X_features_test)
@@ -150,13 +161,15 @@ if method != "cluster":
     print("Finished feature training")
 
 if method == "both":
-    y_pred_train_concat = np.concatenate((y_pred_train1.reshape((-1, 1)), y_pred_train2.reshape((-1, 1))), axis=1)
-    y_pred_test_concat = np.concatenate((y_pred_test1.reshape((-1, 1)), y_pred_test2.reshape((-1, 1))), axis=1)
+    y_pred_train_concat = np.concatenate(
+        (y_pred_train1.reshape((-1, 1)), y_pred_train2.reshape((-1, 1))), axis=1)
+    y_pred_test_concat = np.concatenate(
+        (y_pred_test1.reshape((-1, 1)), y_pred_test2.reshape((-1, 1))), axis=1)
     logreg = LogisticRegression()
     logreg.fit(y_pred_train_concat, y_train)
     y_pred_train = logreg.predict(y_pred_train_concat)
     y_pred_test = logreg.predict(y_pred_test_concat)
-    print(logreg.coef_) 
+    print(logreg.coef_)
 elif method == "cluster":
     y_pred_train = y_pred_train1
     y_pred_test = y_pred_test1
@@ -175,5 +188,5 @@ if method != "cluster":
     output = []
     for i, x in enumerate(x_init_test):
         output.append([X_features_test[i], x_init_test[i], y_pred_test[i]])
-    pickle.dump(output, open(f"trajectory_features_labeled_{dataset}.pkl", "wb"))
-
+    pickle.dump(output, open(
+        f"trajectory_features_labeled_{dataset}.pkl", "wb"))
--- ProposedApproach/Utils.py
+++ ProposedApproach/Utils.py
@@ -1,12 +1,12 @@
 import numpy as np
-
+import geopy.distance as distance
 def average_length_of_sequences(sequences):
     s = sum([len(x) for x in sequences])
     avg = s / len(sequences)
     return round(avg, 2)
 
-def distance_of_trajectory(traj):
+def distance_of_trajectory(traj, isCoordinates=False):
     dist = 0.0
     for i in range(len(traj) - 1):
-        dist += np.linalg.norm(traj[i]-traj[i+1])
+        dist += np.linalg.norm(traj[i]-traj[i+1]) if isCoordinates==False else distance.great_circle(traj[i][::-1], traj[i+1][::-1]).meters
     return dist
