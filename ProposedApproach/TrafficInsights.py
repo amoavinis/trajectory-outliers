a35dc7a2014670424a59ced960cbeaf832dac704
Angelos Moavinis <amoavinis@gmail.com>
2022-11-06 18:14:37 +0200

    options

--- ProposedApproach/TrafficInsights.py
+++ ProposedApproach/TrafficInsights.py
@@ -16,9 +16,15 @@ parser.add_argument("--dataset",
                     default="geolife")
 parser.add_argument(
     "--K", help="The K parameter of the K-Means algorithm.", default="5")
+parser.add_argument("--plot_nn_outliers",
+                    help="Show a plot of the outliers and their nearest neighbors (0 for no, 1 for yes).", default="0")
+parser.add_argument("--plot_manual_outliers",
+                    help="Show a plot of the manually added outliers (0 for no, 1 for yes).", default="0")
 args = parser.parse_args()
 dataset = args.dataset
 K = int(args.K)
+plot_nn_outliers = args.plot_nn_outliers == "1"
+plot_manual_outliers = args.plot_manual_outliers == "1"
 
 data = pickle.load(open("trajectory_features_labeled_"+dataset+".pkl", "rb"))
 
@@ -32,11 +38,12 @@ _, neighbors = knn_model.kneighbors(X)
 outlier_indices = [i for i, x in enumerate(y) if x == 1]
 sampled = sample(outlier_indices, 10)
 
-if dataset == "cyprus":
+if plot_manual_outliers and dataset == "cyprus":
     manual_outliers = pickle.load(open("manual_outliers.pkl", "rb"))
     cmap = colors.ListedColormap(np.random.rand(len(manual_outliers), 3))
     for i in range(len(manual_outliers)):
-        plt.plot([p[0] for p in manual_outliers[i]], [p[1] for p in manual_outliers[i]], color=cmap.colors[i], label="Outlier "+str(i+1))
+        plt.plot([p[0] for p in manual_outliers[i]], [
+                 p[1] for p in manual_outliers[i]], color=cmap.colors[i], label="Outlier "+str(i+1))
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
     plt.legend()
@@ -51,11 +58,12 @@ def plot_outlier(x, neighbors_dict):
     plt.show()
 
 
-for id in sampled:
-    neighbors_dict = {}
-    for neighbor in neighbors[id][1:]:
-        neighbors_dict[neighbor] = [paths[neighbor], y[neighbor]]
-    #plot_outlier(paths[id], neighbors_dict)
+if plot_nn_outliers:
+    for id in sampled:
+        neighbors_dict = {}
+        for neighbor in neighbors[id][1:]:
+            neighbors_dict[neighbor] = [paths[neighbor], y[neighbor]]
+        plot_outlier(paths[id], neighbors_dict)
 
 
 def clustering(data, paths, e):
@@ -64,7 +72,8 @@ def clustering(data, paths, e):
 
     print(e, "-", silhouette_score(data, y_pred))
 
-    for class_id in range(e):
+    # Uncomment below lines to plot starting and ending coordinates of trajectories.
+    """ for class_id in range(e):
         class_data = [data[i]
                       for i in range(len(data)) if y_pred[i] == class_id]
         points = []
@@ -76,7 +85,7 @@ def clustering(data, paths, e):
     plt.xlabel("Longitude")
     plt.ylabel("Latitude")
     plt.legend()
-    plt.show()
+    plt.show() """
 
     colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
     labels_used = set()
