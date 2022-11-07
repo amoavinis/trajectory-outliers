003414babe73c85d5810c358d1428928fa9dc959
Angelos Moavinis <amoavinis@gmail.com>
2022-11-07 22:43:19 +0200

    requirements

--- ProposedApproach/GPXParser.py
+++ ProposedApproach/GPXParser.py
@@ -1,10 +1,10 @@
-from copyreg import pickle
 import gpxpy
 import os
 import pickle
 
-dir = os.getcwd()+"/ProposedApproach/gpx_files/"
-gpx_array = [gpxpy.parse(open(dir+file, 'r')) for file in os.listdir(dir)]
+dir = os.getcwd()+"/ProposedApproach/manual_outliers/"
+gpx_array = [gpxpy.parse(open(dir+file, 'r')) for file in sorted(os.listdir(dir))]
+print(sorted(os.listdir(dir)))
 trajectories = [gpx.tracks[0].segments[0].points for gpx in gpx_array]
 trajectories = [[[p.longitude, p.latitude] for p in gpx] for gpx in trajectories]
 pickle.dump(trajectories, open('manual_outliers.pkl', "wb"))
