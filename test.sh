28e2359670724e9a03a2d5d7f18c83bf0cc44571
Angelos Moavinis <aggelos@istognosis.com>
2022-11-22 23:57:02 +0200

    commands

--- test.sh
+++ test.sh
@@ -1,4 +1,25 @@
+# GEOLIFE AUTO LABELING
+python3 Proposed/AutomaticLabeling.py --dataset geolife
+
+# CYPRUS AUTO LABELING
+python3 Proposed/AutomaticLabeling.py --dataset geolife --G 10
+
+# GEOLIFE CLASSIFICATION
+python3 TOP/runTOP.py --dataset geolife
+python3 DODB/DODB.py --dataset geolife
+python3 ProposedApproach/Proposed.py --dataset geolife --method clustering --minPts 5 --distance_fn hausdorff --G 20
+python3 ProposedApproach/Proposed.py --dataset geolife --method clustering --minPts 2 --distance_fn dtw --G 20
+python3 ProposedApproach/Proposed.py --dataset geolife --method clustering --minPts 5 --distance_fn dtw_hilbert --G 20
 python3 ProposedApproach/Proposed.py --dataset geolife --method svm --do_gsp 0
 python3 ProposedApproach/Proposed.py --dataset geolife --method svm --do_gsp 1
+python3 ProposedApproach/Proposed.py --dataset geolife --method both --minPts 5 --distance_fn hausdorff --G 20 --do_gsp 1
+
+# CYPRUS CLASSIFICATION
+python3 TOP/runTOP.py --dataset cyprus --seqGap 6 --minSup 3
+python3 DODB/DODB.py --dataset cyprus -W 2 -D1 30000
+python3 ProposedApproach/Proposed.py --dataset cyprus --method clustering --minPts 30 --distance_fn hausdorff --G 40
+python3 ProposedApproach/Proposed.py --dataset cyprus --method clustering --minPts 5 --distance_fn dtw --G 40
+python3 ProposedApproach/Proposed.py --dataset cyprus --method clustering --minPts 2 --distance_fn dtw_hilbert --G 40
 python3 ProposedApproach/Proposed.py --dataset cyprus --method svm --do_gsp 0
 python3 ProposedApproach/Proposed.py --dataset cyprus --method svm --do_gsp 1
+python3 ProposedApproach/Proposed.py --dataset cyprus --method both --minPts 30 --distance_fn hausdorff --G 40 --do_gsp 1
